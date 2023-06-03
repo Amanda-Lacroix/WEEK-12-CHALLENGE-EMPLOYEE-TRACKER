301f18c61d68858898028bd35546b56d636a2825
Beezyz <116973964+Beezyz@users.noreply.github.com>
2023-06-02 22:00:15 -0300

    added to the switch statements, created functions for the queries, created function to callback to the options list for queries. Trying to resolve duplication ofprompt issue

--- server.js
+++ server.js
@@ -9,20 +9,44 @@ sqlConnection.query('SELECT * FROM department', (err, results) => {
   if (err) {
     console.error('Error executing query:', err);
   };
-  console.log('Results:');
-  console.table(results);
+  // Shows the table
+   console.table(results);
+  // Takes user back to the Options List
+   optionsList();
   });
 };
 
 
+// Function to Query the role
+function viewRole() {
+  sqlConnection.query('SELECT * FROM role', (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+    };
+    console.table(results);
+    optionsList();
+    });
+  };
+
+  // Function to Query the employees
+function viewEmployee() {
+  sqlConnection.query('SELECT * FROM employee', (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+    };
+    console.table(results);
+    optionsList()
+    });
+  };
   
 // Initial Prompt for list of options
+function optionsList() {
 inquirer.prompt ([
       {
         name: `options`,
         type: `list`,
         message: "Please make a selection:",
-        choices: [`View All Departments`,`View All Roles`,`View All Employees`,`Add a Department`, 'Add a Role', 'Add an Employee', 'Update an Employee Role'],
+        choices: [`View All Departments`,`View All Roles`,`View All Employees`,`Add a Department`, 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Exit'],
 
     },
   ])
@@ -31,10 +55,19 @@ inquirer.prompt ([
 .then((answers) => {
  switch (answers.options){
     case 'View All Departments':
-    viewDepartment();
+      viewDepartment();
     break;
+    case 'View All Roles':
+      viewRole();
+    break;
+    case 'View All Employees':
+      viewEmployee();
+    break;
+    // exit the connection
     case 'Exit':
-      inquirer.prompt();
+      sqlConnection.end();
     break;
  }});
+}
 
+optionsList();
